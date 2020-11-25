475c76f33272fde9269019f1f28016805d9efa26
Mohammed-khamis <moh.o.khamis@gmail.com>
2020-11-25 12:29:28 +0200

    creating search form

---
+++ src/components/SearchForm.js
@@ -0,0 +1,35 @@
+import React, { useRef, useEffect } from 'react';
+
+const SearchForm = ({ setSearchTerm }) => {
+	const searchValue = useRef('');
+
+	useEffect(() => {
+		searchValue.current.focus();
+	}, []);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
+	const searchMovie = () => {
+		setSearchTerm(searchValue.current.value);
+	};
+	return (
+		<section className="section">
+			<h2 className="section-title">search movies</h2>
+			<form className="form search-form" onSubmit={handleSubmit}>
+				<div className="form-control">
+					<label htmlFor="name">search your favorite movie</label>
+					<input
+						type="text"
+						name="name"
+						id="name"
+						onChange={searchMovie}
+						ref={searchValue}
+					/>
+				</div>
+			</form>
+		</section>
+	);
+};
+
+export default SearchForm;
