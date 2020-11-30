012f4a6ca317dd473d3e9bafafb268146d9812a2
Mohammed-khamis <moh.o.khamis@gmail.com>
2020-11-30 08:24:18 +0200

    make the logo clickable

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,9 @@ const Navbar = () => {
 	return (
 		<nav className="navbar">
 			<div className="nav-center">
-				<img src={logo} alt="movies logo" className="logo" />
+				<Link to="/">
+					<img src={logo} alt="movies logo" className="logo" />
+				</Link>
 				<ul className="nav-links">
 					<li>
 						<Link to="/"> Home </Link>
